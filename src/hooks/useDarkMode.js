96752f6567c26d88377f3f3e27cae4aa6f5a2b24
Vickie Nelson <vickie.isabel.nelson@gmail.com>
2020-06-03 16:39:44 -0500

    dark mode error fixed and displaying but error logging that mode on line 13 in useDarkMode is imssing a depencency

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,44 @@
+import { useEffect } from "react";
+import { useLocalStorage } from "./useLocalStorage";
+
+export const useDarkMode = (initialValue) => {
+  const [mode, setMode] = useLocalStorage("light", initialValue);
+  const test = document.querySelector("body");
+  useEffect(() => {
+    if (mode) {
+      test.classList.add("dark-mode");
+    } else {
+      test.classList.remove("dark-mode");
+    }
+  }, [mode]);
+  return [mode, setMode];
+};
+
+// import React, { useEffect } from "react";
+// import { useLocalStorage } from "./useLocalStorage";
+
+// export const useDarkMode = (initialValue) => {
+//   //const[whatmodeis,settingthemode] = useLocalStorage(c,d)
+//   //const test = document.querySelector('body')
+//   //useEffect to set the state
+//   // useEffect(()=>{
+//   //     // if statement that adds dark
+//   //     if (){
+
+//   //     } else{ //removes dark
+//   //     }
+//   // }, [f])
+//   // return [whatmodeis,settingthemode]
+
+//   const [mode, setMode] = useLocalStorage("lightMode", initialValue);
+//   const isBody = document.querySelector("body");
+
+//   useEffect(() => {
+//     if (mode) {
+//       isBody.classList.add("darkMode");
+//     } else {
+//       isBody.classList.remove("darkMode");
+//     }
+//   }, [mode]);
+//   return [mode, setMode];
+// };
